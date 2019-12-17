c8eb7deed742019b05e286e2d09c38780bce6e93
Ruslan <ruzyqwerty@gmail.com>
2019-12-17 16:55:04 +0500

    Added preload and ESC is exit

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
-from level import Level
 from settings import WINDOW_SIZE, FULLSCREEN, FPS
-
 
 pygame.init()
 if FULLSCREEN:
     screen = pygame.display.set_mode(flags=pygame.FULLSCREEN | pygame.RESIZABLE)
 else:
     screen = pygame.display.set_mode(WINDOW_SIZE)
+
+from level import Level
+
 screen.fill((255, 255, 255))
 clock = pygame.time.Clock()
 level = Level(10, screen)
@@ -17,6 +18,8 @@ while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
         # TODO На Esc можно сделать смену режима окна (полный/неполный экран), я не придумал как чтобы все работало
         # elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
