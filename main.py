4c2125328b3ac51135e2eba599fe73b960743ac9
ruzyqwerty <ruzyqwerty@gmail.com>
2020-01-12 12:30:46 +0500

    Do settings fullscreen mode

--- main.py
+++ main.py
@@ -28,9 +28,12 @@ cursor.image = AIM[0]
 cursor.rect = cursor.image.get_rect()
 all_sprites.add(cursor)
 
+rooms_count = ROOMS_COUNT
+last_fullscreen = FULLSCREEN
+
 screen.fill((255, 255, 255))
 clock = pygame.time.Clock()
-level = Level(ROOMS_COUNT, screen)
+level = Level(rooms_count, screen)
 
 on_pause = False
 start_menu = True
@@ -54,6 +57,19 @@ while running:
 
     if on_pause or start_menu:
         menu.update(events)
+        if menu.settings.fullscreen != last_fullscreen:
+            last_fullscreen = menu.settings.fullscreen
+            if menu.settings.fullscreen:
+                screen = pygame.display.set_mode(flags=pygame.FULLSCREEN | pygame.RESIZABLE)
+            else:
+                screen = pygame.display.set_mode(WINDOW_SIZE)
+            for sprite in menu.buttons_sprites:
+                sprite.change_name(sprite.text)
+            for sprite in menu.setting_sprites:
+                sprite.change_name(sprite.text)
+            menu.surface = screen
+            interface.surface = screen
+            level.surface = screen
         if 'exit' in menu.events:
             menu.events.remove('exit')
             running = False
@@ -63,15 +79,15 @@ while running:
             on_pause = False
         if 'new game' in menu.events:
             menu.events.remove('new game')
-            level = Level(ROOMS_COUNT, screen)
+            level = Level(rooms_count, screen)
             on_pause = False
         menu.render()
     else:
         if level.isLevelEnd:
             player = level.player
             room_done = level.room_done
-            ROOMS_COUNT += 1
-            level = Level(ROOMS_COUNT, screen, player)
+            rooms_count += 1
+            level = Level(rooms_count, screen, player)
             level.room_done = room_done
         level.update(events)
         update_data()
