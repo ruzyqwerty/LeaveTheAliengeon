564fdd36e7de292dc1d32dcaa7bed044ab8125d8
EVa_3 <andreyka0309@gmail.com>
2020-01-13 23:05:25 +0500

    ---

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ BLOCK_SIZE = 50     # default = 50
 # Скорость пули
 BULLET_SPEED = 5   # default depended of window size = 5 but independed = 20
 # Скорость игрока
-PLAYER_SPEED = 2    # default depended of window size = 1.2 but independed = 5
+PLAYER_SPEED = 2    # default depended of window size = 2 but independed = 5
 # Скорость врагов
 ENEMY_SPEED = 1
 # Стартовое кол-во комнат
